49d3b861c5a3253588b3f35c0d40e4b6b7ddf57e
Linh Bui <linh@Linhs-iMac.local>
2022-09-16 01:03:27 -0700

    set and alert character limit and type

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ var generateBtn = document.querySelector("#generate");
 
 // Define generarePassword function
 function generatePassword(){
-  const result = prompt("your password is generating.."); //Create Prompts
+  const result = parseInt(prompt("What do you the password length to be?"),10); //Modified and converted Prompt's content 
 
+  if (result >= 8 && result<= 128) {   // set character limit
+  } else if (Number.isNaN(result)){
+    alert("You must enter a number"); // alert variable type must be numeric
+    return null;
+  } else {
+    alert("Password length must be at least 8 characters and no more than 128 characters"); // alert not in range 
+    return null; 
+  } 
 }
 
 // Write password to the #password input
