064f6ed1cb39d2e351d41da5a91628cecc579021
Linh Bui <linh@Linhs-iMac.local>
2022-09-15 23:52:15 -0700

    Define generate pw and add prompt

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Define generarePassword function
+function generatePassword(){
+  const result = prompt("your password is generating.."); //Create Prompts
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
